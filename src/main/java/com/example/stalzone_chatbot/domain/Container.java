8bc9f7c4b148ea65971604ac8129781af5992702
Alexander Hansen <alexanderhansen124@gmail.com>
2026-09-22 15:11:27 +0700

    feat: add container entities

---
+++ src/main/java/com/example/stalzone_chatbot/domain/Container.java
@@ -0,0 +1,13 @@
+package com.example.stalzone_chatbot.domain;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor 
+public class Container {
+    float effectiveness;
+    int capacity;
+    float protection;
+    //need something for the optional stats, maybe a map of stat name to value
+    
+
+}
